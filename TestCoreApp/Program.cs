db2b7ccf59d87c775fa81ed358f913605524dade
selvaparur <mail2meselva@gmail.com>
2020-01-06 19:32:22 +0530

    Update Program.cs
    test command

--- TestCoreApp/Program.cs
+++ TestCoreApp/Program.cs
@@ -14,6 +14,7 @@ namespace TestCoreApp
     {
         public static void Main(string[] args)
         {
+            // Added for test command
             CreateWebHostBuilder(args).Build().Run();
         }
 
